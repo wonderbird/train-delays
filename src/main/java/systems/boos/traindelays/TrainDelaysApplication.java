b5338ab00584a09ea197c30d44c8d6cf74947fff
Stefan Boos <kontakt@boos.systems>
2022-03-29 19:29:10 +0200

    feat: generate initial spring boot project
    ... using https://start.spring.io/

---
+++ src/main/java/systems/boos/traindelays/TrainDelaysApplication.java
@@ -0,0 +1,13 @@
+package systems.boos.traindelays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainDelaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainDelaysApplication.class, args);
+	}
+
+}
