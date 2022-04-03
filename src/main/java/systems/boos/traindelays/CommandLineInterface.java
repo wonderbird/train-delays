889c54d7d66b6b0325b3139ef88947d27555ac1e
Stefan Boos <kontakt@boos.systems>
2022-04-03 20:34:02 +0200

    test: (wip) use mock-server for integration tests

--- src/main/java/systems/boos/traindelays/CommandLineInterface.java
+++ src/main/java/systems/boos/traindelays/CommandLineInterface.java
@@ -2,13 +2,22 @@ package systems.boos.traindelays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommandLineInterface {
     private static final Logger logger = LoggerFactory.getLogger(CommandLineInterface.class);
 
+    private final TimetablesService timetablesService;
+
+    @Autowired
+    public CommandLineInterface(TimetablesService timetablesService) {
+        this.timetablesService = timetablesService;
+    }
+
     public void run() {
+        // TODO Timetable timetable = timetablesService.fetchChanges();
         logger.info("Next train is scheduled to leave at {}", "00:00");
     }
 }
--- src/test/java/systems/boos/traindelays/cucumber/steps/TrainDelaysCucumberStepDefinitions.java
+++ src/test/java/systems/boos/traindelays/cucumber/steps/TrainDelaysCucumberStepDefinitions.java
@@ -7,6 +7,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.mockserver.integration.ClientAndServer;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import systems.boos.traindelays.CommandLineInterface;
@@ -14,20 +15,19 @@ import systems.boos.traindelays.cucumber.common.MemoryAppender;
 
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.matchesPattern;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 public class TrainDelaysCucumberStepDefinitions {
 
     private static final String LOGGER_NAME = "systems.boos.traindelays";
-
-    // Cucumber's mechanism of wiring is not known to IntelliJ
+    private static ClientAndServer mockServer;
+    private static MemoryAppender memoryAppender;
+    // Cucumber's mechanism of wiring is not known to IntelliJ. Thus, we suppress the warning issued by IntelliJ.
     @SuppressWarnings("SpringJavaAutowiredMembersInspection")
     @Autowired
     private CommandLineInterface cli;
-
-    private static MemoryAppender memoryAppender;
 
     @Before
     public static void setupLogger() {
@@ -39,6 +39,16 @@ public class TrainDelaysCucumberStepDefinitions {
         memoryAppender.start();
     }
 
+    @Before
+    public void startMockServer() {
+        mockServer = ClientAndServer.startClientAndServer(8085);
+    }
+
+    @After
+    public void stopMockServer() {
+        mockServer.stop();
+    }
+
     @After
     public void resetAndStopLogger() {
         memoryAppender.reset();
@@ -47,7 +57,28 @@ public class TrainDelaysCucumberStepDefinitions {
 
     @When("^I run the application$")
     public void i_run_the_application() {
+        configureMockServer();
         cli.run();
+    }
+
+    private void configureMockServer() {
+        mockServer
+                .when(
+                        request()
+                                .withMethod("GET")
+                                .withPath("/timetables/v1/fchg/8005143")
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withHeader("Content-Type", "application/xml")
+                                .withBody("""
+                                        <timetable>
+                                        <s>
+                                            <dp ct="2204020000" />
+                                        </s>
+                                        </timetable>""")
+                );
     }
 
     @Then("I should see {string} as scheduled departure time for the next train")
