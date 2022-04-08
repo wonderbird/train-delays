92b36f77488d318fcb5d1608ccb8c3e381741d7a
Stefan Boos <kontakt@boos.systems>
2022-04-08 20:55:41 +0200

    test: fix mockserver api endpoint configuration

--- src/test/java/systems/boos/traindelays/cucumber/common/CucumberSpringConfiguration.java
+++ src/test/java/systems/boos/traindelays/cucumber/common/CucumberSpringConfiguration.java
@@ -2,8 +2,10 @@ package systems.boos.traindelays.cucumber.common;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@TestPropertySource("classpath:test.properties")
 public class CucumberSpringConfiguration {
 }
