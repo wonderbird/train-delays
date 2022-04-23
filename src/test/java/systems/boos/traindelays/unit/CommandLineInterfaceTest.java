0696c4931ebf5de65423304918a837c7ce247dc9
Stefan Boos <kontakt@boos.systems>
2022-04-23 21:27:39 +0200

    refactor(tests): simplify CommandLineInterfaceTest

--- src/test/java/systems/boos/traindelays/unit/CommandLineInterfaceTest.java
+++ src/test/java/systems/boos/traindelays/unit/CommandLineInterfaceTest.java
@@ -9,22 +9,19 @@ import org.springframework.web.client.RestClientException;
 import systems.boos.traindelays.CommandLineInterface;
 import systems.boos.traindelays.TimetablesService;
 import systems.boos.traindelays.common.MemoryAppender;
-import systems.boos.traindelays.model.Event;
 import systems.boos.traindelays.model.Timetable;
-import systems.boos.traindelays.model.TimetableStop;
 
-import java.time.Clock;
 import java.time.Instant;
-import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class CommandLineInterfaceTest {
     private MemoryAppender memoryAppender;
-    private final String scheduleMessagePrefix = "Next train is scheduled to leave at ";
 
 
     @BeforeEach
@@ -38,26 +35,19 @@ class CommandLineInterfaceTest {
         memoryAppender.stop();
     }
 
-    // TODO change this test so that it uses a stub for the Timetable
     @Test
-    void Run_SinglePastTimetableStopInResponse_DoesNotLogDepartureTime() {
-        Clock clock = givenItIs("10:00");
-
-        Timetable response = new Timetable();
-        response.setTimetableStops(List.of(stopWithDeparture("08:00")));
+    void Run_NoFutureDepartureScheduled_LogsNoFutureDeparturesMessage() {
+        Timetable response = mock(Timetable.class);
+        when(response.findFirstDepartureAfter(any(Instant.class))).thenReturn(Optional.empty());
 
         TimetablesService serviceStub = mock(TimetablesService.class);
         when(serviceStub.fetchChanges()).thenReturn(response);
 
         // WHEN I run the command line interface
         CommandLineInterface sut = new CommandLineInterface(serviceStub);
-        sut.setClock(clock);
         sut.run();
 
-        // THEN no next departure is logged
-        assertNoLogEntry(scheduleMessagePrefix, Level.INFO);
-
-        // AND a message about no future departures is logged
+        // THEN a message about no future departures is logged
         assertSingleLogEntry("No future train departures available", Level.WARN);
     }
 
@@ -74,43 +64,9 @@ class CommandLineInterfaceTest {
         assertSingleLogEntry("The Deutsche Bahn OpenAPI Portal reported an error", Level.ERROR);
     }
 
-    private Clock givenItIs(String time) {
-        String nowString = String.format("2022-04-09T%s:00+02:00", time);
-        return Clock.fixed(Instant.parse(nowString), ZoneId.of("Europe/Berlin"));
-    }
-
-    // TODO move stopWithDeparture to TimetableTest
-    private TimetableStop stopWithDeparture(String expectedDepartureTime) {
-        String changedTimeString = String.format("2022-04-09T%s:00+02:00", expectedDepartureTime);
-        Instant changedTime = Instant.parse(changedTimeString);
-
-        Event departure = new Event();
-        departure.setChangedTime(changedTime);
-
-        TimetableStop stop = new TimetableStop();
-        stop.setDepartures(List.of(departure));
-        return stop;
-    }
-
-    private void assertLogEntryWithDeparture(String expectedDepartureTime) {
-        List<ILoggingEvent> events = memoryAppender.search(scheduleMessagePrefix, Level.INFO);
-        assertEquals(1, events.size(), "departure time should be logged once");
-
-        // AND the expected departure time is logged
-        String actualDepartureTime = (String) events.get(0).getArgumentArray()[0];
-        assertEquals(expectedDepartureTime, actualDepartureTime);
-    }
-
-    private void assertNumberOfLogEntries(String substring, Level level, int expectedOccurrences, String message) {
-        List<ILoggingEvent> events = memoryAppender.search(substring, level);
-        assertEquals(expectedOccurrences, events.size(), message);
-    }
-
-    private void assertNoLogEntry(String substring, Level level) {
-        assertNumberOfLogEntries(substring, level, 0, "message should not be logged");
-    }
-
     private void assertSingleLogEntry(String substring, Level level) {
-        assertNumberOfLogEntries(substring, level, 1, "message should be logged once");
+        List<ILoggingEvent> events = memoryAppender.search(substring, level);
+        assertEquals(1, events.size(), "message should be logged once");
     }
+
 }
