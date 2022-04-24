3ff3f88e97557bc6cac673de9eec7f7a283dc570
Stefan Boos <kontakt@boos.systems>
2022-04-24 15:55:49 +0200

    test: re-add spring boot contextLoads test

---
+++ src/test/java/systems/boos/traindelays/TrainDelaysApplicationTests.java
@@ -0,0 +1,20 @@
+package systems.boos.traindelays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class TrainDelaysApplicationTests {
+
+	@Autowired
+	private TrainDelaysApplication trainDelaysApplication;
+
+	@Test
+	void contextLoads() {
+		assertNotNull(trainDelaysApplication);
+	}
+
+}
