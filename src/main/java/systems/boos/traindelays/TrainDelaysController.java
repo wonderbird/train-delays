b75db14962af3fb6a17208203550b7f77bec195e
Stefan Boos <kontakt@boos.systems>
2022-05-22 18:49:46 +0200

    feat: handle situation that no departure forecast is present

--- src/main/java/systems/boos/traindelays/TrainDelaysController.java
+++ src/main/java/systems/boos/traindelays/TrainDelaysController.java
@@ -1,10 +1,12 @@
 package systems.boos.traindelays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import systems.boos.traindelays.model.Timetable;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.Optional;
@@ -19,12 +21,20 @@ public class TrainDelaysController {
     }
 
     @GetMapping("/nextdeparture")
-    public ExpectedDepartureResponse getNextDeparture() {
+    public ExpectedDepartureResponse getNextDeparture(HttpServletResponse response) {
         Timetable timetable = timetablesService.fetchChanges();
         Optional<Instant> nextDeparture = timetable.findFirstDepartureAfter(Instant.now());
 
-        return new ExpectedDepartureResponse(nextDeparture.get().atZone(ZoneOffset.UTC));
+        ExpectedDepartureResponse result;
+        if (nextDeparture.isPresent()) {
+            result = new ExpectedDepartureResponse(nextDeparture.get().atZone(ZoneOffset.UTC));
+        } else {
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+            result = new ExpectedDepartureResponse(null);
+        }
 
-        // TODO Handle edge cases: no next departure, exception from timetables service (e.g. status 500, not authorized because wrong api key)
+        return result;
+
+        // TODO Handle edge cases: exception from timetables service (e.g. status 500, not authorized because wrong api key)
     }
 }
