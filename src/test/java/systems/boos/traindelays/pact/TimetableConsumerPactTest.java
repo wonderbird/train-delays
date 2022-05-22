e93d424b4f007f32b349ac43de56ddf7badf9524
Stefan Boos <kontakt@boos.systems>
2022-05-22 18:54:56 +0200

    test(pact): handle invalid api token for timetable service

--- src/test/java/systems/boos/traindelays/pact/TimetableConsumerPactTest.java
+++ src/test/java/systems/boos/traindelays/pact/TimetableConsumerPactTest.java
@@ -11,6 +11,7 @@ import org.apache.commons.collections4.MapUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 import systems.boos.traindelays.TimetablesService;
 import systems.boos.traindelays.model.Event;
@@ -23,12 +24,46 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonBody;
 import static au.com.dius.pact.consumer.dsl.Matchers.timestamp;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(PactConsumerTestExt.class)
 class TimetableConsumerPactTest {
     Instant arbitraryInstant = Instant.parse("2022-04-01T19:49:00Z");
+
+    @Pact(consumer = "FrontendApplication", provider = "TimetableService")
+    V4Pact invalidTimetableApiToken(PactBuilder builder) {
+        return builder
+                .usingLegacyDsl()
+                .given("the timetable api token is invalid")
+                .uponReceiving("fetch changes for station with eva 8005143")
+                .method("GET")
+                .path("/fchg/8005143")
+                .willRespondWith()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .headers(jsonContentTypeHeader())
+                .body(newJsonBody(root -> {
+                    root.object("error", error -> {
+                       error.integerType("code", 900901);
+                       error.stringType("message", "Invalid Credentials");
+                       error.stringType("description", "Access failure. Unauthorized.");
+                    });
+                }).build())
+                .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "invalidTimetableApiToken")
+    void findChanges_whenApiTokenIsInvalid(MockServer mockServer) {
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .rootUri(mockServer.getUrl())
+                .build();
+
+        Timetable actual = new TimetablesService(restTemplate).fetchChanges();
+
+        assertEquals(0, actual.getTimetableStops().size());
+    }
 
     @Pact(consumer = "FrontendApplication", provider = "TimetableService")
     V4Pact departureIsPresent(PactBuilder builder) {
@@ -44,7 +79,7 @@ class TimetableConsumerPactTest {
                 .path("/fchg/8005143")
                 .willRespondWith()
                 .status(200)
-                .headers(headers())
+                .headers(xmlContentTypeHeader())
                 .body(new PactXmlBuilder("timetable")
                         .build(root ->
                                 root.eachLike("s", 1, Collections.emptyMap(), timetableStop ->
@@ -54,29 +89,9 @@ class TimetableConsumerPactTest {
                 .toPact(V4Pact.class);
     }
 
-    @Pact(consumer = "FrontendApplication", provider = "TimetableService")
-    V4Pact departureIsNotPresent(PactBuilder builder) {
-        String apiDateTimePattern = "yyMMddHHmm";
-        String apiResponseChangedTime = arbitraryInstant.atZone(ZoneId.of("Europe/Berlin"))
-                .format(DateTimeFormatter.ofPattern(apiDateTimePattern));
-
-        return builder
-                .usingLegacyDsl()
-                .given("no departures for station with eva 8005143 present")
-                .uponReceiving("fetch changes for station with eva 8005143")
-                .method("GET")
-                .path("/fchg/8005143")
-                .willRespondWith()
-                .status(200)
-                .headers(headers())
-                .body(new PactXmlBuilder("timetable")
-                        .build(root -> {}))
-                .toPact(V4Pact.class);
-    }
-
     @Test
     @PactTestFor(pactMethod = "departureIsPresent")
-    void findChanges_whenDepartureForStationWithEvaIsPresent(MockServer mockServer) {
+    void findChanges_whenDepartureIsPresent(MockServer mockServer) {
         RestTemplate restTemplate = new RestTemplateBuilder()
                 .rootUri(mockServer.getUrl())
                 .build();
@@ -90,9 +105,25 @@ class TimetableConsumerPactTest {
         assertEquals(expected, actual.getTimetableStops().get(0).getDepartures().get(0));
     }
 
+    @Pact(consumer = "FrontendApplication", provider = "TimetableService")
+    V4Pact departureIsNotPresent(PactBuilder builder) {
+        return builder
+                .usingLegacyDsl()
+                .given("no departures for station with eva 8005143 present")
+                .uponReceiving("fetch changes for station with eva 8005143")
+                .method("GET")
+                .path("/fchg/8005143")
+                .willRespondWith()
+                .status(200)
+                .headers(xmlContentTypeHeader())
+                .body(new PactXmlBuilder("timetable")
+                        .build(root -> {}))
+                .toPact(V4Pact.class);
+    }
+
     @Test
     @PactTestFor(pactMethod = "departureIsNotPresent")
-    void findChanges_whenDepartureForStationWithEvaIsNotPresent(MockServer mockServer) {
+    void findChanges_whenDepartureIsNotPresent(MockServer mockServer) {
         RestTemplate restTemplate = new RestTemplateBuilder()
                 .rootUri(mockServer.getUrl())
                 .build();
@@ -100,6 +131,14 @@ class TimetableConsumerPactTest {
         Timetable actual = new TimetablesService(restTemplate).fetchChanges();
 
         assertEquals(0, actual.getTimetableStops().size());
+    }
+
+    private Map<String, String> xmlContentTypeHeader() {
+        return mapOf("Content-Type", "application/xml");
+    }
+
+    private Map<String, String> jsonContentTypeHeader() {
+        return mapOf("Content-Type", "application/json");
     }
 
     private <T> Map<String, T> mapOf(String key, T value) {
@@ -113,9 +152,5 @@ class TimetableConsumerPactTest {
     private Map<String, Object> mapOf(String key1, Object value1, String key2, Object value2, String key3, Object value3,
                                       String key4, Object value4, String key5, Object value5) {
         return MapUtils.putAll(new HashMap<>(), new Object[]{key1, value1, key2, value2, key3, value3, key4, value4, key5, value5});
-    }
-
-    private Map<String, String> headers() {
-        return mapOf("Content-Type", "application/xml");
     }
 }
